93673f38cef959240564d6dc32d25113c7955f51
John Walley <john@walley.org.uk>
2020-06-27 16:43:39 +0100

    feat: initial version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function haveISeenThisNumber(number, seenBeforeList) {
+  let seenBefore = false;
+
+  seenBeforeList.forEach((entry) => {
+    if (entry === number) {
+      seenBefore = true;
+    }
+  });
+
+  return seenBefore;
+}
 
 function App() {
+  let [currentNumber, setSelectedNumber] = useState(99);
+  let [seenBeforeList, setSeenBeforeList] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {currentNumber === 99
+            ? "Let's Bingo"
+            : currentNumber === 98
+            ? "Bingo!"
+            : currentNumber}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => {
+            if (seenBeforeList.length < 90) {
+              let newNumber = Math.floor(90 * Math.random()) + 1;
+
+              while (haveISeenThisNumber(newNumber, seenBeforeList)) {
+                newNumber = Math.floor(90 * Math.random()) + 1;
+              }
+
+              setSeenBeforeList([...seenBeforeList, newNumber]);
+              setSelectedNumber(newNumber);
+            } else {
+              setSelectedNumber(98);
+            }
+          }}
+          style={{ fontSize: "48px" }}
         >
-          Learn React
-        </a>
+          next number
+        </button>
       </header>
     </div>
   );
